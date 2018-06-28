5a6f8e0bd7570d13480792577503e41307b183c3
doetools <seweitian@gmail.com>
2018-06-28 12:07:07 -0400

    add the compiling files for isat lib which is based on CUs compiling strcuture

---
+++ compile/isat/SOURCES.mk
@@ -0,0 +1,14 @@
+# Get the name of files in a directory and its subdirectories
+# Author: Wei Tian <Wei.Tian@Schneider-Electric.com>
+FILES=ellu_bbt2chol.f90 ellu_chol2eig.f90 ellu_pt_near_far.f90 ellu_radii.f90 ellu_rad_upper.f90    \ 
+	ell_aff_pr.f90 ell_bbt2chol.f90 ell_bbt2eig.f90 ell_chol2eig.f90 ell_chol_det.f90    \ 
+	ell_cov_2d.f90 ell_eig2chol.f90 ell_full2eig.f90 ell_full2low.f90 ell_house.f90    \ 
+	ell_line_proj.f90 ell_low2chol.f90 ell_pair_cover.f90 ell_pair_cover_cv.f90 ell_pair_cover_cv_ql.f90    \ 
+	ell_pair_cover_it.f90 ell_pair_cover_query.f90 ell_pair_cover_sph.f90 ell_pair_separate.f90 ell_pair_shrink.f90    \ 
+	ell_pts_uncover.f90 ell_pt_dist.f90 ell_pt_hyper.f90 ell_pt_in.f90 ell_pt_modify.f90    \ 
+	ell_pt_near_far.f90 ell_pt_shrink.f90 ell_radii.f90 ell_rad_lower.f90 ell_rad_upper.f90    \ 
+	isatab.f90 isat_abort_ser.f90 isat_cdf.f90 isat_cdf_action.f90 isat_change_m.f90    \ 
+	isat_defaults.f90 isat_init.f90 isat_io.f90 isat_kill.f90 isat_lu.f90    \ 
+	isat_lu_m.f90 isat_mpi_ser.f90 isat_mpi_type.f90 isat_prec.f90 isat_rnu.f90    \ 
+	isat_subs.f90 isat_sys_unix.f90 isat_types.f90 isat_val.f90 id_list.f90    \ 
+	sell_bt.f90 sell_ebt.f90 sell_ll.f90 sell_m.f90 
